9d27b4cb6ddbceacaec998fd8d575aa64b9ff36b
Bryan Wilhite <rasx@songhaysystem.com>
2020-05-06 15:40:20 -0700

    #2 :hammer: `IssueManagerContext` parameterless ctor issue

--- Edu.Ucsb.IssueManager/IssueManagerContext.cs
+++ Edu.Ucsb.IssueManager/IssueManagerContext.cs
@@ -1,22 +1,17 @@
 ﻿using Edu.Ucsb.IssueManager.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Edu.Ucsb.IssueManager
 {
     public class IssueManagerContext : DbContext
     {
-        public IssueManagerContext(string connectionString)
-        {
-            this._connectionString = connectionString;
-        }
-
         public DbSet<Issue> Issues { get; set; }
 
         public DbSet<UserIssue> UserIssues { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(this._connectionString);
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
 
-        private readonly string _connectionString;
+        internal IConfiguration Configuration { get; }
     }
 }
